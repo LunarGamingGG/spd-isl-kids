83657038c78c84cd1a4d6e2c69e1490fb4bf40b6
LunarGamingGG <101939019+LunarGamingGG@users.noreply.github.com>
2026-04-25 19:08:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
-import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
@@ -12,9 +12,12 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     tsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tanstackStart(),
     viteReact(),
   ],
-})
+  build: {
+    outDir: "dist"
+  }
+});
